c680b6e6c07d3c8ebf74e1e5b032dcf6f41d080e
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-08-11 22:57:47 +0300

    Updates

---
+++ precache-manifest.72af7f311015081f4401288377f199be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae5d818f357a0b77d00130b8928e788",
+    "url": "/SashaKumeiko.github.io/slack-clone/index.html"
+  },
+  {
+    "revision": "758eeaacd33a388adf6a",
+    "url": "/SashaKumeiko.github.io/slack-clone/static/css/main.a7c2144d.chunk.css"
+  },
+  {
+    "revision": "eb3cd7e4cced0289ae33",
+    "url": "/SashaKumeiko.github.io/slack-clone/static/js/2.3b5478a1.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/SashaKumeiko.github.io/slack-clone/static/js/2.3b5478a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758eeaacd33a388adf6a",
+    "url": "/SashaKumeiko.github.io/slack-clone/static/js/main.529e35b4.chunk.js"
+  },
+  {
+    "revision": "80ce3b0c72ed10795915",
+    "url": "/SashaKumeiko.github.io/slack-clone/static/js/runtime-main.8f494bd1.js"
+  }
+]);
